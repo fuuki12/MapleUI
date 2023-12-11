6362e2e64ff6563ba3a13916b90df5ab1f10773f
fuki_fuki <fuuki1228@icloud.com>
2023-12-12 00:38:48 +0900

    firstcommit

---
+++ src/components/CircleSpinner.svelte
@@ -0,0 +1,30 @@
+<script>
+	let size = 50;
+	let color = '#000000';
+</script>
+
+<div
+	class="circle-spinner"
+	style="
+      width: {size}px;
+      height: {size}px;
+      border: 2px solid {color};
+      border-top-color: transparent;
+  "
+/>
+
+<style>
+	.circle-spinner {
+		border-radius: 50%;
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
